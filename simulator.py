07f602063369c292f5caa4d614cb27d33d06a78d
Shayan Tehrani <shayanthrn@gmail.com>
2021-02-28 14:52:49 +0330

    adding base 16 to code

--- simulator.py
+++ simulator.py
@@ -1,5 +1,8 @@
 import xlrd
-
+import xlwt
+import sys
+import time
+from xlutils.copy import copy
 
 def comparision(prefix1,prefix2):
 	if(len(prefix1)==len(prefix2)):
@@ -34,13 +37,40 @@ def binarySearch (arr, l, r, x):
     else:
         return l,"failed"
 
+def hex2bin(hexval): 
+    value = int(hexval, 16) 
+    bindigits = [] 
+     
+    # Seed digit: 2**0 
+    digit = (value % 2) 
+    value //= 2 
+    bindigits.append(digit) 
+     
+    while value > 0: 
+        # Next power of 2**n 
+        digit = (value % 2) 
+        value //= 2 
+        bindigits.append(digit) 
+         
+    return ''.join([str(d) for d in bindigits])
+
+
 
 class root:
     def __init__(self):
         self.prefix="root"
 
 class prefixnode:
-    def __init__(self,mylist):
+    def __init__(self,mylist,base):
+        if(base==16):
+            if(type(mylist[0])==float):
+                mylist[0]=int(mylist[0])
+            mylist[0]=str(mylist[0])
+            mylist[0]=hex2bin(mylist[0])[::-1]
+            while(len(mylist[0])!=32):
+                mylist[0]="0"+mylist[0]
+            mylist[0]=mylist[0][:int(mylist[1])]
+            mylist=[mylist[0]+"*",int(mylist[2])]
         self.prefix=mylist[0][:-1]
         self.nexthopdict={str(len(self.prefix)):int(mylist[1])}  #pathinformation
         self.nexthop=int(mylist[1])
@@ -53,15 +83,20 @@ class prefixnode:
 listofips=[]
 
 # Give the location of the file
-loc = ("routingtable.xls")
- 
+location=input("please enter the path of routing table:\n")
+loc = (location)
+
+base=int(input("routing table is base of 2 or 16:\n"))
+
 # To open Workbook
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
  
-for i in range(sheet.nrows):
-    listofips.append(prefixnode(sheet.row_values(i)))
 
+start_time = time.time_ns()
+
+for i in range(sheet.nrows):
+    listofips.append(prefixnode(sheet.row_values(i),base))
 
 #sort routing enteries
 
@@ -90,8 +125,7 @@ for i in listofips:
     if(i.isleaf==1):
         listofleaves.append(i)
 
-
-
+timetobuild=time.time_ns() - start_time
 
 def getnexthop(index,listofips,address):
     matchlen=0
@@ -102,9 +136,11 @@ def getnexthop(index,listofips,address):
         matchlen-=1
     if(matchlen==0):
         print("The default gatway is next hop\n")
+        return "default gayway"
     else:
         nexthop=listofips[index].nexthopdict[str(matchlen)]
         print("matched with: ",listofips[index].prefix[:matchlen]," ","the next hop is : ",nexthop)
+        return str(nexthop)
     
 def getnexthopdis(index,listofleaves,address):
     matchlen1=0
@@ -139,12 +175,18 @@ def getnexthopdis(index,listofleaves,address):
 
 print("list of prefixes:")
 for i in range(len(listofips)):
-    print(i,"-",listofips[i].prefix)
+    print(i+1,"-",listofips[i].prefix)
 print("-------------")
 print("list of leaves:")
 for i in range(len(listofleaves)):
-    print(i,"-",listofleaves[i].prefix)
+    print(i+1,"-",listofleaves[i].prefix)
 print("-------------")
+print("number of entries: ",len(listofips))
+print("size of structure:(all nodes)")
+print(sys.getsizeof(listofips)," Bytes")
+print("size of structure:(only leaves)")
+print(sys.getsizeof(listofleaves)," Bytes")
+print("time to build structure: ",timetobuild," nanoseconds")
 # testcase="01101111000011110000111100001111"  next hop is 8
 # testcase="10110111000000000000000000000000"  next hop is 2
 # testcase="10111111000000000000000000000000"  next hop is 8
@@ -153,7 +195,7 @@ print("-------------")
 # testcase="10000111000000000000000000000000"  next hop is 3
 
 while(True):
-    choice=input("1-searching for address\n2-searching for address(disjoint)\n3-exit\n")
+    choice=input("1-searching for address\n2-searching for address(disjoint)\n3-select csv file and analyse\n4-exit\n")
     if(choice=="1"):
         address=input("please enter the 32 bit ip address:\n")
         index,status = binarySearch(listofips, 0, len(listofips)-1,address)
@@ -172,6 +214,49 @@ while(True):
             print("The default gatway is next hop\n")
         else:
             getnexthopdis(index,listofleaves,address)
+    elif(choice=="3"):
+        path=input("please enter path of csv file:(base of tests should be 16)\n")
+        loc2 = (path)
+        # To open Workbook
+        wb2 = xlrd.open_workbook(loc2)
+        sheet2 = wb2.sheet_by_index(0)
+        w = copy(wb2)
+        choice2=input("1-binary search 2-binary search using disjoint prefixes\n")
+        start=time.time_ns()
+        if(choice2=="1"):
+            for i in range(sheet2.nrows):
+                if(type(sheet2.row_values(i)[0])==float):
+                    address=hex2bin(str(int(sheet2.row_values(i)[0])))[::-1]
+                else:
+                    address=hex2bin(str(sheet2.row_values(i)[0]))[::-1]
+                while(len(address)!=32):
+                    address="0"+address
+                index,status = binarySearch(listofips, 0, len(listofips)-1,address)
+                if(status=="found"):
+                    w.get_sheet(0).write(i,1,str(listofips[index].nexthop))
+                elif(index==len(listofips)):
+                    w.get_sheet(0).write(i,1,"default gatway")
+                else:
+                    w.get_sheet(0).write(i,1,getnexthop(index,listofips,address))
+        else:
+            for i in range(sheet2.nrows):
+                if(type(sheet2.row_values(i)[0])==float):
+                    address=hex2bin(str(int(sheet2.row_values(i)[0])))[::-1]
+                else:
+                    address=hex2bin(str(sheet2.row_values(i)[0]))[::-1]
+                while(len(address)!=32):
+                    address="0"+address
+                index,status = binarySearch(listofleaves, 0, len(listofleaves)-1,address)
+                if(status=="found"):
+                    w.get_sheet(0).write(i,1,str(listofleaves[index].nexthop))
+                elif(index==len(listofleaves)):
+                    w.get_sheet(0).write(i,1,"default gatway")
+                else:
+                    w.get_sheet(0).write(i,1,getnexthop(index,listofleaves,address))
+        w.save('answer.xls')
+        end=time.time_ns()
+        print("\n\n\n\n\n\n\n\n\n\n\n")
+        print("number of entries: ",sheet2.nrows)
+        print("execution time: ",end-start," nanoseconds")
     else:
         break
-
